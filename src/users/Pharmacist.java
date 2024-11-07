fc10ac6c4449bd596f1eb6ae4d7242ff09328884
epilog04 <rizqyharith04@gmail.com>
2024-11-07 19:26:32 +0800

    Add files via upload

--- src/users/Pharmacist.java
+++ src/users/Pharmacist.java
@@ -1,41 +1,40 @@
 package users;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import appointments.AppointmentOutcomeRecord;
-import inventory.Inventory;
-import medicalrecords.Prescription;
-
 public class Pharmacist extends User {
-    private List<AppointmentOutcomeRecord> appointmentOutcomeRecords;
+    private List<AppointmentOutcomeRecord> appointmentOutcomeRecords; // Need update prescription class
     private Inventory inventory;
 
-    public Pharmacist(String id, String name, String dateOfBirth, String gender, String phoneNumber,
-            String emailAddress, String password) {
-        super(id, name, "PHARMACIST", password, phoneNumber, emailAddress, dateOfBirth, gender);
+    public Pharmacist(String id, String name, String password) {
+        super(id, name, "PHARMACIST", password);
         this.appointmentOutcomeRecords = new ArrayList<>();
         this.inventory = new Inventory();
+    }
+
+    @Override
+    public void viewProfile() {
+        System.out.println("Pharmacist Profile: " + getName() + " (ID: " + getId() + ")");
     }
 
     public void viewAppointmentOutcomeRecords() {
         for (AppointmentOutcomeRecord record : appointmentOutcomeRecords) {
             System.out.println("Appointment Date: " + record.getAppointmentDate());
-            System.out.println("Service Type: " + record.getServiceProvided());
+            System.out.println("Service Type: " + record.getServiceType());
             System.out.println("Consultation Notes: " + record.getConsultationNotes());
             System.out.println("Prescribed Medications:");
-            for (Prescription medication : record.getPrescriptions()) {
+            for (Prescription medication : record.getPrescribedMedications()) {
                 System.out.println(" - " + medication);
             }
             System.out.println();
         }
     }
 
-    public void updatePrescriptionStatus(String medicationName, int newStatus) {
+    public void updatePrescriptionStatus(String medicationName, String newStatus) {
         for (AppointmentOutcomeRecord record : appointmentOutcomeRecords) {
-            for (Prescription medication : record.getPrescriptions()) {
-                if (medication.getMedicationName().equals(medicationName)) {
+            for (Prescription medication : record.getPrescribedMedications()) {
+                if (medication.getName().equals(medicationName)) {
                     medication.setStatus(newStatus);
                     System.out.println("Updated status for " + medicationName + " to " + newStatus);
                     return;
@@ -46,20 +45,15 @@ public class Pharmacist extends User {
     }
 
     public void viewMedicationInventory() {
-        inventory.displayInventory();
+        inventory.displayStockLevels();
     }
 
     public void submitReplenishmentRequest(String medicationId, int quantity) {
-        if (inventory.isLow(medicationId, quantity)) {
-            System.out.println(
-                    "Replenishment request submitted for " + quantity + " units of medication ID: " + medicationId);
+        if (inventory.isLow(medicationId)) {
+            System.out.println("Replenishment request submitted for " + quantity + " units of medication ID: " + medicationId);
         } else {
             System.out.println("Stock levels are sufficient.");
         }
-    }
-
-    public void addAppointmentOutcomeRecord(AppointmentOutcomeRecord record) {
-        this.appointmentOutcomeRecords.add(record);
     }
 
 }
