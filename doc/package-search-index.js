e75712e10311cf1b258d142b4e7184d8b728d44a
bliblublob <huangyuxuan121122@gmail.com>
2024-11-17 21:52:37 +0800

    Docs: Add generated javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"appointments"},{"l":"database"},{"l":"main"},{"l":"managers"},{"l":"medicalrecords"},{"l":"medicine"},{"l":"menus"},{"l":"menus.utils"},{"l":"users"}];updateSearchResults();
